ef3121a12fee34f439278bd0b164fa1e7c4fb320
yang aaa <yangfancheng1@gmail.com>
2023-07-19 23:42:19 -0400

    Apply styling to thread list

--- src/Main.js
+++ src/Main.js
@@ -22,7 +22,7 @@ const Main = ({ handleSignOut, loggedIn }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
     try {
       const payload = {
         username: loggedIn.username,
@@ -41,53 +41,54 @@ const Main = ({ handleSignOut, loggedIn }) => {
 
   return (
     <div style={{ marginTop: "20px" }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" style={{ flexGrow: 1 }}>
-            Thread App
-          </Typography>
-          <Button color="inherit" onClick={handleSignOut}>Sign Out</Button>
-        </Toolbar>
-      </AppBar>
-      
-        <Container>
-          <form onSubmit={handleSubmit} style={{ marginBottom: "20px" }}>
-            <TextField
-              label="Title"
-              variant="outlined"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              fullWidth
-              required
-              style={{ marginBottom: "10px" }}
-            />
-            <TextField
-              label="Description"
-              variant="outlined"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              multiline
-              minRows={4}
-              fullWidth
-              required
-              style={{ marginBottom: "10px" }}
-            />
-            <Button type="submit" variant="contained" color="primary">
-              Add Thread
-            </Button>
-          </form>
 
-          <List>
-            {threads.map((thread) => (
-              <ListItem key={thread.id}>
-                <ListItemText
-                  primary={thread.title}
-                  secondary={thread.description}
-                />
-              </ListItem>
-            ))}
-          </List>
-        </Container>
+      <Container>
+        <form onSubmit={handleSubmit} style={{ marginBottom: "20px" }}>
+          <TextField
+            label="Title"
+            variant="outlined"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            fullWidth
+            required
+            style={{ marginBottom: "10px" }}
+          />
+          <TextField
+            label="Description"
+            variant="outlined"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            multiline
+            minRows={4}
+            fullWidth
+            required
+            style={{ marginBottom: "10px" }}
+          />
+          <Button type="submit" variant="contained" color="primary">
+            Add Thread
+          </Button>
+        </form>
+
+        <List style={{ marginTop: "20px" }}>
+          {threads.map((thread) => (
+            <ListItem key={thread.id} alignItems="flex-start" style={{ border: "1px solid #ccc", borderRadius: "4px", marginBottom: "10px", padding: "10px" }}>
+              <ListItemText
+                primary={<Typography variant="h6" style={{ fontWeight: "bold" }}>{thread.title}</Typography>}
+                secondary={
+                  <React.Fragment>
+                    <Typography component="span" variant="body2" style={{ color: "#666" }}>
+                      {thread.description}
+                    </Typography>
+                    <Typography component="span" variant="caption" style={{ display: "block", marginTop: "5px", color: "#999" }}>
+                      Posted by {thread.username}
+                    </Typography>
+                  </React.Fragment>
+                }
+              />
+            </ListItem>
+          ))}
+        </List>
+      </Container>
 
     </div>
   );
