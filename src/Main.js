7fc934d4a6239e14fabf572cbe5aad8a7cea29b7
YangfanCheng1 <32468503+YangfanCheng1@users.noreply.github.com>
2023-07-23 00:06:00 -0400

    Add tabs for selecting thread type

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { Container, AppBar, Toolbar, Typography, Button, TextField, List, ListItem, ListItemText } from "@material-ui/core";
+import {
+  Container,
+  Tabs,
+  Tab,
+  Typography, Button, TextField, List, ListItem, ListItemText
+} from "@material-ui/core";
 import axios from "axios";
 
 const Main = ({ handleSignOut, loggedIn }) => {
   const [threads, setThreads] = useState([]);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [activeTab, setActiveTab] = useState(0);
 
   useEffect(() => {
     fetchThreads();
@@ -41,7 +47,6 @@ const Main = ({ handleSignOut, loggedIn }) => {
 
   return (
     <div style={{ marginTop: "20px" }}>
-
       <Container>
         <form onSubmit={handleSubmit} style={{ marginBottom: "20px" }}>
           <TextField
@@ -69,25 +74,32 @@ const Main = ({ handleSignOut, loggedIn }) => {
           </Button>
         </form>
 
-        <List style={{ marginTop: "20px" }}>
-          {threads.map((thread) => (
-            <ListItem key={thread.id} alignItems="flex-start" style={{ border: "1px solid #ccc", borderRadius: "4px", marginBottom: "10px", padding: "10px" }}>
-              <ListItemText
-                primary={<Typography variant="h6" style={{ fontWeight: "bold" }}>{thread.title}</Typography>}
-                secondary={
-                  <React.Fragment>
-                    <Typography component="span" variant="body2" style={{ color: "#666" }}>
-                      {thread.description}
-                    </Typography>
-                    <Typography component="span" variant="caption" style={{ display: "block", marginTop: "5px", color: "#999" }}>
-                      Posted by {thread.username}
-                    </Typography>
-                  </React.Fragment>
-                }
-              />
-            </ListItem>
-          ))}
-        </List>
+        <div>
+          <Tabs value={activeTab} onChange={(event, newValue) => setActiveTab(newValue)} indicatorColor="primary" textColor="primary" centered>
+            <Tab label="Latest" />
+            <Tab label="Popular" />
+            <Tab label="For you" />
+          </Tabs>
+          <List style={{ marginTop: "20px" }}>
+            {threads.map((thread) => (
+              <ListItem key={thread.id} alignItems="flex-start" style={{ border: "1px solid #ccc", borderRadius: "4px", marginBottom: "10px", padding: "10px" }}>
+                <ListItemText
+                  primary={<Typography variant="h6" style={{ fontWeight: "bold" }}>{thread.title}</Typography>}
+                  secondary={
+                    <React.Fragment>
+                      <Typography component="span" variant="body2" style={{ color: "#666" }}>
+                        {thread.description}
+                      </Typography>
+                      <Typography component="span" variant="caption" style={{ display: "block", marginTop: "5px", color: "#999" }}>
+                        Posted by {thread.username}
+                      </Typography>
+                    </React.Fragment>
+                  }
+                />
+              </ListItem>
+            ))}
+          </List>
+        </div>
       </Container>
 
     </div>
