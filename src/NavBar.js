8f0a74a508c60201042358fdbf91f6675390096b
yang aaa <yangfancheng1@gmail.com>
2023-08-02 23:37:59 -0400

    Add navbar section

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,19 +4,22 @@ import { Link } from "react-router-dom";
 
 const NavBar = ({ loggedIn, handleSignOut }) => {
   return (
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" style={{ flexGrow: 1 }}>
-            My App
-          </Typography>
-          <Typography variant="body1" style={{ marginRight: "10px" }}>
-            Welcome, {loggedIn ? "Username" : ""}
-          </Typography>
-          <Button color="inherit" onClick={handleSignOut}>
-            Sign Out
-          </Button>
-        </Toolbar>
-      </AppBar>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
+          My App
+        </Typography>
+        <Button color="inherit" component={Link} to="/thread">
+          Thread
+        </Button>
+        <Typography variant="body1" style={{ marginRight: "10px" }}>
+          Welcome, {loggedIn ? "Username" : ""}
+        </Typography>
+        <Button color="inherit" onClick={handleSignOut}>
+          Sign Out
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 };
 
